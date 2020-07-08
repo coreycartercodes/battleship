76a0eb32524f3ef52ab5705c752f2c536d03169b
Corey Carter <crc2231@gmail.com>
2020-07-08 16:06:37 -0600

    Add board initialize - only 1 cell

--- lib/board.rb
+++ lib/board.rb
@@ -1,1 +1,17 @@
 # Board Class Setup
+
+class Board
+
+  attr_reader :cells
+
+  def initialize
+    @cells = {
+      A1: Cell.new('A1')
+    }
+  end
+
+  def valid_coordinate?(coordinate_param)
+    @cells.keys.to_s.include?(coordinate_param)
+  end
+
+end
