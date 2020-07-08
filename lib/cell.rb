6efd40088ee5e8d3a51e1ad5d16f1a4438abf84b
Corey Carter <crc2231@gmail.com>
2020-07-08 00:11:36 -0600

    Add render cell render method and test

--- lib/cell.rb
+++ lib/cell.rb
@@ -21,7 +21,46 @@ class Cell
   end
 
   def fire_upon
-    @fired_upon = true
+    case ship
+    when nil
+              #May need to add "Miss" here
+      @fired_upon = true
+    else
+      @fired_upon = true
+      @ship.hit
+    end
+
   end
+
+def render_fire_upon_helper
+  case ship
+  when nil
+    p "M"
+  else
+    if ship.sunk?
+      p "X"
+    else
+      p "H"
+    end
+  end
+end
+
+  def render(show_ships = false)
+    case @fired_upon
+    when false
+      if ship == nil
+        p "."
+      else
+        if show_ships == true
+          p "S"
+        else
+          p "."
+        end
+      end
+    else
+    render_fire_upon_helper
+    end
+  end
+
 
 end
--- test/cell_test.rb
+++ test/cell_test.rb
@@ -8,6 +8,8 @@ require 'pry'
 class CellTest < Minitest::Test
   def setup
     @cell1 = Cell.new("B4")
+    @cell2 = Cell.new("C2")
+    @cruiser = Ship.new("Cruiser", 3)
   end
 
   def test_it_exists
@@ -23,8 +25,8 @@ class CellTest < Minitest::Test
   end
 
   def test_place_ship
-    @cell1.place_ship("Cruiser")
-    assert_equal "Cruiser", @cell1.ship
+    @cell1.place_ship(@cruiser)
+    assert_equal "Cruiser", @cell1.ship.name
   end
 
   def test_empty
@@ -35,6 +37,39 @@ class CellTest < Minitest::Test
     assert_equal false, @cell1.fired_upon?
     @cell1.fire_upon
     assert_equal true, @cell1.fired_upon?
+  end
+
+  def test_health_fire_upon
+    @cell1.place_ship(@cruiser)
+    assert_equal 3, @cell1.ship.health
+
+    @cell1.fire_upon
+    assert_equal 2, @cell1.ship.health
+  end
+
+  def test_render         #Should we break this up?
+
+    @cell1.place_ship(@cruiser)
+    assert_equal ".", @cell1.render
+
+    assert_equal "S", @cell1.render(true)
+
+    @cell1.fire_upon
+
+    assert_equal "H", @cell1.render
+
+    @cell1.fire_upon
+    @cell1.fire_upon
+    assert_equal "X", @cell1.render
+
+    assert_equal ".", @cell2.render
+
+    assert_equal ".", @cell2.render(true)
+
+    @cell2.fire_upon
+
+    assert_equal "M", @cell2.render
+
   end
 
 end
