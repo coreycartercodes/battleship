1c10648d7948864cb0e90da8e290bf25d1a095d2
Carson Jardine <carsonjardine@gmail.com>
2020-07-07 20:38:44 -0600

    Add cell tests

--- test/cell_test.rb
+++ test/cell_test.rb
@@ -1,1 +1,28 @@
 #Cell Class Test
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ship'
+require './lib/cell'
+require 'pry'
+
+class CellTest < Minitest::Test
+  def setup
+    @cell = Cell.new("B4")
+  end
+
+  def test_it_exists
+    assert_instance_of Cell, @cell
+  end
+
+  def test_coordinate
+    assert_equal "B4", @cell.coordinate
+  end
+
+  def test_ship
+    assert_nil @cell.ship
+  end
+
+  def test_empty
+    assert_equal true, @cell.empty?
+  end
+end
