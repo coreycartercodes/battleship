fc6bde5d6d0ace76fcfe846fe8e616568c965ddf
Carson Jardine <carsonjardine@gmail.com>
2020-07-07 20:31:22 -0600

    Remove comments

--- test/ship_test.rb
+++ test/ship_test.rb
@@ -9,27 +9,26 @@ class ShipTest < Minitest::Test
     @cruiser = Ship.new("Cruiser", 3)
   end
 
-  #test it exists
   def test_it_exists
     assert_instance_of Ship, @cruiser
   end
-  #test it has a name
+
   def test_it_has_name
     assert_equal "Cruiser", @cruiser.name
   end
-  #test the length of the Ship
+
   def test_length
     assert_equal 3, @cruiser.length
   end
-  #test the health of the ship
+
   def test_health
     assert_equal 3, @cruiser.health
   end
-  #test if it has sunk?
+
   def test_if_it_has_sunk
     assert_equal false, @cruiser.sunk?
   end
-  #test it has been hit
+
   def test_health_change_after_hit
     @cruiser.hit
 
