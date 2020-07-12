c2eb3b657cb9bc2749f6238c0c7bd350f9f5419f
Carson Jardine <carsonjardine@gmail.com>
2020-07-12 12:48:30 -0600

    Add message class

--- lib/game.rb
+++ lib/game.rb
@@ -1,3 +1,27 @@
+require 'pry'
+
 class Game
+
+  # def initialize
+  #
+  # end
+
+  def main_menu
+    # need to pull in welcome_message from Message class
+    input = gets.chomp.downcase
+
+    if input == "p"
+      game_setup
+    else
+
+    end
+  end
+
+  def game_setup
+  end
+
+
+
+
 
 end
--- test/game_test.rb
+++ test/game_test.rb
@@ -7,6 +7,18 @@ require './lib/game'
 require 'pry'
 
 class GameTest < Minitest::Test
-  
+
+  def setup
+    game = Game.new
+  end
+
+  def test_it_exists
+    assert_instance_of Game, game
+  end
+
+  def test_welcome_message
+
+    assert_equal
+  end
 
 end
