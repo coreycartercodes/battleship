7d912319fe7d0907f3f86f19f2f042295ac78875
Corey Carter <crc2231@gmail.com>
2020-07-12 22:33:41 -0600

    Add and test render and board placement

--- battleship_runner.rb
+++ battleship_runner.rb
@@ -4,14 +4,11 @@ require './lib/board'
 require './lib/game'
 
 game = Game.new
+
 puts game.computer.board.render
 
 puts game.player.board.render
 
 game.computer.cpu_ship_placement
 
-puts game.player.board.render(show_ships = true)
-
-#
-# @number_range = (1..@columns).to_a
-# @letter_range = ("A"..(("A".ord)+ @rows - 1).chr).to_a
+puts game.computer.board.render(show_ships = true)
