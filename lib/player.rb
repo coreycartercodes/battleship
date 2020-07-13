26653540105b75c02b70a4516f7c40835a5947ab
Corey Carter <crc2231@gmail.com>
2020-07-13 09:52:47 -0600

    Use keys.shuffle for cpu_ship_placement

--- lib/player.rb
+++ lib/player.rb
@@ -13,19 +13,9 @@ class Player
   end
 
   def cpu_ship_placement
-    cpu_place = Array.new(3)
-    cpu_place[0] = @board.cells.keys.shuffle[0]
-    randomizer = rand(1000)
-
-    if randomizer.even?
-      cpu_place[1] = "#{cpu_place[0][0] + (cpu_place[0][1]+1).to_s}"
-      cpu_place[2] = "#{cpu_place[0][0] + (cpu_place[0][1]+2).to_s}"
-    else
-      cpu_place[1] = "#{(cpu_place[0][0].ord+1).chr + cpu_place[0][1].to_s}"
-      cpu_place[2] = "#{(cpu_place[0][0].ord+2).chr + cpu_place[0][1].to_s}"
-    end
-    if @board.valid_placement?(@cruiser, cpu_place)
-      @board.place(@cruiser, cpu_place)
+    @cpu_place = @board.cells.keys.shuffle[0..2]
+    if @board.valid_placement?(@cruiser, @cpu_place)
+      @board.place(@cruiser, @cpu_place)
     else
       cpu_ship_placement
     end
