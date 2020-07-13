beffd2f608e3d6af9eac35d620f5fbc109b16170
Corey Carter <crc2231@gmail.com>
2020-07-13 14:13:29 -0600

    Begin order of game

--- battleship_runner.rb
+++ battleship_runner.rb
@@ -9,12 +9,12 @@ game = Game.new
 
 game.main_menu
 
-### USED TO TEST RENDERING
-puts game.computer.board.render
-
-puts game.player.board.render
-
 game.computer.cpu_ship_placement
 
-puts game.computer.board.render(show_ships = true)
-###
+game.player_place_ships
+
+game.player_cruiser
+
+game.player_submarine
+
+game.turn
