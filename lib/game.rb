5ce71fc3937a6bcb355500d0f70751e7dd0a1840
Carson Jardine <carsonjardine@gmail.com>
2020-07-12 22:16:30 -0600

    Add emojis

--- lib/game.rb
+++ lib/game.rb
@@ -17,19 +17,20 @@ class Game
   end
 
   def main_menu
-    p "Welcome to BATTLESHIP"
-    p "Enter p to play. Enter q to quit."
-    print "> "
+    puts "Welcome to BATTLESHIP"
+    puts "Enter p to play. Enter q to quit."
+    print "\u{1f6a2}  "
     input = gets.chomp.downcase
     if input == 'p'
       #computer_place_ships
       player_place_ships
       #turn
     elsif input == 'q'
+      puts "Okay, quitter \u{1f644}"
       exit
     else
-      p "You don't listen to directions, huh? Try again."
-      sleep 4.0 
+      puts  "You don't listen to directions, huh? Try again."
+      puts "   "
       main_menu
     end
   end
@@ -44,14 +45,14 @@ class Game
 
   def player_cruiser
     puts "Enter the squares for the Cruiser (3 spaces): "
-    print "> "
+    print "\u{1f6a2}  "
     cruiser_input = gets.chomp.upcase.split(" ")
     if @player_board.valid_placement?(@player_ships["Cruiser"], cruiser_input)
       @player_board.place(@player_ships["Cruiser"], cruiser_input)
       puts @player_board.render(true)
     else
       puts "Those are invalid coordinates. Please try again: "
-      print "> "
+      print "\u{1f6a2}  "
       player_cruiser
     end
     player_submarine
@@ -59,19 +60,33 @@ class Game
 
   def player_submarine
     puts "Enter the squares for the Submarine (2 spaces): "
-    print "> "
+    print "\u{1f6a2}  "
     submarine_input = gets.chomp.upcase.split(" ")
     if @player_board.valid_placement?(@player_ships["Submarine"], submarine_input)
       @player_board.place(@player_ships["Submarine"], submarine_input)
       puts @player_board.render(true)
     else
       puts "Those are invalid coordinates. Please try again: "
-      print "> "
+      print "\u{1f6a2}  "
       player_submarine
     end
   end
 
+  def turn
+    display_boards
 
+  end
 
+  def display_boards
+    puts "=============COMPUTER BOARD============="
+    #puts @computer_board.render
+    puts "                "
+    puts "==============PLAYER BOARD=============="
+    puts @player_board.render(true)
+  end
+
+  # def player_turn
+  #   puts
+  # end
 
 end
