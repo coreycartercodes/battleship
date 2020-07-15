3137b19dd9332e11b16beb8122834cb3b1dcead3
Carson Jardine <carsonjardine@gmail.com>
2020-07-15 11:51:42 -0600

    Remove pry and messages, add comments for potential tests to run

--- lib/game.rb
+++ lib/game.rb
@@ -1,4 +1,3 @@
-require 'pry'
 require './lib/player'
 
 class Game
@@ -53,7 +52,7 @@ class Game
     end
     player_submarine
   end
-  #### It is placing 2 submarines again.
+
   def player_submarine
     puts "Enter the squares for the Submarine (2 spaces): "
     print "\u{1f6a2}  "
